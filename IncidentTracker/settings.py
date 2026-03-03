ff8e9e25e163ac0ad5d251392394231ecb39ba1e
sergilaredo <sergi.laredo.joliver@gmail.com>
2026-03-03 19:05:27 +0100

    Add logging and fixture dirs for debugging

--- IncidentTracker/settings.py
+++ IncidentTracker/settings.py
@@ -134,3 +134,28 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Configuració de LOGGING per veure errors a la consola de GitHub Actions
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+    },
+}
+
+# Perquè el test trobi la fixture testdb.json a l'arrel
+FIXTURE_DIRS = [BASE_DIR]
